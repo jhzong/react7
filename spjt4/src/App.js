a8cde6da118df73516ffba172477bd1f5728b30b
jhzong <jhzong05@gmail.com>
2026-01-06 11:19:52 +0900

    useState를 이용해 입력정보 바로출력

--- spjt4/src/App.js
+++ spjt4/src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+import './css/Style.css';
 
 function App() {
+
+  const[id,setId]=useState("");
+  const[name,setName]=useState("");
+  
+  const [txtId,setTxtId]=useState("");
+  const [txtName,setTxtName]=useState("");
+  
+  const tBtn=()=>{
+    alert("아이디와 이름을 출력합니다.");
+    setTxtId(id);
+    setTxtName(name);
+    // 입력창 초기화
+    setId("");
+    setName("");
+  }
+
+  const txtKeyUp=(e)=>{
+    if(e.keyCode===13){
+      tBtn();
+    }
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="root">
+      <div className="txtName">{txtId} {txtName}</div>
+      <input type="text" placeholder="아이디를 입력하시오." value={id} onKeyUp={txtKeyUp} onChange={(e)=>{setId(e.target.value)}}/>
+      <input type="text" placeholder="이름을 입력하시오." value={name} onKeyUp={txtKeyUp} onChange={(e)=>{setName(e.target.value)}}/>
+      <button onClick={tBtn}>확인</button>
     </div>
   );
 }
