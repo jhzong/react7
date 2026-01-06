96019d77e9d5d75905d1dc36ffd61219a1db5b61
jhzong <jhzong05@gmail.com>
2026-01-06 10:51:20 +0900

    useEffect

--- spjt3/src/App.js
+++ spjt3/src/App.js
@@ -4,13 +4,28 @@ import './App.css';
 import './css/Style.css';
 
 function App() {
+  
   // useState 변수: 값이 변경되면 화면 갱신(reload)
   const [cnt, setCnt] = useState(0);
+  const [id, setId] = useState('aaa');
   // useRef 변수: 값이 변경되어도 화면 갱신 없음. reload되어도 값을 유지.
   // useRef : current키에 값을 저장 => cntRef.current
   const cntRef = useRef(10);
   // 일반 변수: 화면 갱신(reload)되면 값 초기화.
   let count = 0;
+
+  // useEffect: 최초 실행시 구동
+  useEffect(
+    () => {
+    console.log("최초 실행 실행");
+    },[cnt]
+  );
+  // useEffect: 최초 1회 실행
+  useEffect(
+    () => {
+    console.log("최초 실행 실행");
+    },[]
+  );
   
   // useState 값 출력방식
   const cntBtn = () => {
@@ -25,6 +40,9 @@ function App() {
   const countBtn = () => {
     count += 1;
     console.log("count:", count);
+    // useState변수 => reload
+    setId('bbb'+count)
+    console.log("id:", id);
   }
   // useRef 값 출력방식
   const refBtn = () => {
