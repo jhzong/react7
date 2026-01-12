f3db454cda05574176850a1fde6b5d634943a131
jhzong <jhzong05@gmail.com>
2026-01-12 10:46:09 +0900

    리엑트 axios

---
+++ spjt9/src/App.js
@@ -0,0 +1,64 @@
+import logo from './logo.svg';
+import './App.css';
+import './css/Style.css';
+import axios from 'axios'//ajax
+import { useState,useEffect } from 'react';
+
+function App() {
+
+  // JS일반변수 - 변수값을 변경할수 있지만, html에 반영이 안되. 화면갱신 시 리셋
+  const uuuser = 'aaa'
+
+  // useState변수 - 데이터가 변경되면 html에 반영되는 변수
+  const [users,setUsers] = useState([])
+  
+  // 화면 갱신될 때 실행되는 함수
+  useEffect(()=>{
+    axios.get('https://jsonplaceholder.typicode.com/users')
+    .then(res => {
+      console.log('json데이터 : ',res.data)
+      setUsers(res.data)
+    })
+  },[]// [] : 최초 한번만 실행
+  )//useEffect약식
+
+  // useEffect(
+  //   axios({
+  //     method:'get',
+  //     url:'https://jsonplaceholder.typicode.com/users',
+  //     data:{},
+  //   }).then((data)=>{
+  //       console.log('성공')
+  //     }).catch(error=>{
+  //     console.log('실패')
+  //   })
+  // )//useEffect정석
+
+
+
+  const user_list = users.map((user)=>{
+    return(
+      
+      <div className="card" key={user.id}>
+        <h5 className="card-header">번호 : {user.id}</h5>
+        <div className="card-body">
+          <h5 className="card-title">이름 : {user.name}</h5>
+          <p className="card-text">주소 : {user.address}</p>
+          <button className="btn btn-primary">수정</button>
+          <button className="btn btn-primary">삭제</button>
+        </div>
+      </div>
+      
+    )
+
+  })
+
+  return (
+    <div className="root">
+      <h2>회원리스트</h2>
+      {user_list}
+    </div>
+  );
+}
+
+export default App;
